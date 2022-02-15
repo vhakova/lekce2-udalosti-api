d512570614056f7bc879849b37479eeee19353d1
Veronika Háková <veronika.hakova@gmail.com>
2022-02-15 18:52:00 +0100

    classList

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // tady je místo pro náš program
 
 console.log('Jsem nalinkovaný')
+
+let nadpis = document.querySelector('h1');
+
+nadpis.classList.add('none');
+nadpis.classList.remove('cosi');
+/**nadpis.classList.toggle('něco') - přepínátko = když to tam je, tak se to vypne, když to tam není, zapne se to**/
+
+
