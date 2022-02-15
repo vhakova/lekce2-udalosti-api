b7790a6a7b6149fee8676a4ce38b52a874e73225
Veronika Háková <veronika.hakova@gmail.com>
2022-02-15 22:16:37 +0100

    smazáno, co navíc jest

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // tady je místo pro náš program
 
-console.log('Jsem nalinkovaný')
+/**console.log('Jsem nalinkovaný')
 
-/**let nadpis = document.querySelector('h1');
+let nadpis = document.querySelector('h1');
 
 nadpis.classList.add('none');
 nadpis.classList.remove('cosi');
