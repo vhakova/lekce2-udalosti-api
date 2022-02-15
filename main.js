a006c435f9df7a3c4bbcf1b4dc6e4ad8df7a230e
Veronika Háková <veronika.hakova@gmail.com>
2022-02-15 18:59:35 +0100

    ztloustni()

--- main.js
+++ main.js
@@ -8,4 +8,7 @@ nadpis.classList.add('none');
 nadpis.classList.remove('cosi');
 /**nadpis.classList.toggle('něco') - přepínátko = když to tam je, tak se to vypne, když to tam není, zapne se to**/
 
-
+function ztloustni() {
+    let text = document.querySelector('p');
+    text.style.fontWeight = 'bold';
+}
